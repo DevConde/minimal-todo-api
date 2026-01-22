9c92c3988841252b6deadab15383e83815b457cc
André Conde <andreconde99@hotmail.com>
2026-01-22 01:05:02 +0000

    feat: add basic structure

---
+++ MinimalApi.Application/Services/TodoService.cs
@@ -0,0 +1,17 @@
+﻿using MinimalApi.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalApi.Application.Services
+{
+    public class TodoService : ITodoService
+    {
+        // Create
+        // Read
+        // Update
+        // Delete
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Core.Models;
+using MinimalApi.Infrastructure;
+
 namespace MinimalApi
 {
     public class Program
@@ -6,7 +10,8 @@ namespace MinimalApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            // Setup InMemory TodoList database
+            builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
             // Add services to the container.
             builder.Services.AddAuthorization();
 
@@ -24,28 +29,54 @@ namespace MinimalApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+            app.MapGet("/todoitems", async (TodoDb db) =>
+                await db.Todos.ToListAsync());
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            app.MapGet("/todoitems/complete", async (TodoDb db) =>
+                await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+            app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+                await db.Todos.FindAsync(id)
+                    is Todo todo
+                        ? Results.Ok(todo)
+                        : Results.NotFound());
+
+            app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
             {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast")
-            .WithOpenApi();
+                db.Todos.Add(todo);
+                await db.SaveChangesAsync();
+
+                return Results.Created($"/todoitems/{todo.Id}", todo);
+            });
+
+            app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+            {
+                var todo = await db.Todos.FindAsync(id);
+
+                if (todo is null) return Results.NotFound();
+
+                todo.Name = inputTodo.Name;
+                todo.IsComplete = inputTodo.IsComplete;
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+            {
+                if (await db.Todos.FindAsync(id) is Todo todo)
+                {
+                    db.Todos.Remove(todo);
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
+            });
+
 
             app.Run();
         }
